d512b4a1d17e555776a5b2a9ed772cb4f2a1670b
Tanish196 <tanishjagetiya@gmail.com>
2025-08-22 21:56:05 +0530

    Linking Frontend and Backend

--- backend/index.js
+++ backend/index.js
@@ -319,5 +319,9 @@ app.get('/search-note/', authenticateToken, async (req, res) => {
     }
 })
 
-app.listen(8000);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
 module.exports = app
